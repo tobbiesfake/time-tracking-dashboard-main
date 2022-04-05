c5b1954560a84bb683f15a24c4c22d07ec425b15
Tobbies <85476101+tobbiesfake@users.noreply.github.com>
2022-04-05 22:56:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+import { timeData } from "./data.js"
+window.onload = () =>{
+    const curHours = document.querySelectorAll(".current-hour");
+    const prevHours = document.querySelectorAll(".previous-hour");
+    const titles = document.querySelectorAll(".title");
+    const daily = document.querySelector("#daily");
+    const weekly = document.querySelector("#weekly");
+    const monthly = document.querySelector("#monthly");
+
+    (function () {
+      titles.forEach ((title, index) => {
+        title.innerText = timeData[index].title
+      })
+    })()
+
+    function setTimeFrame(timeFrame) {
+      curHours.forEach (function (hour, index) {
+        hour.innerText = `${timeData[index].timeframes[timeFrame].current}hrs`
+      })
+
+      switch(timeFrame){
+        case "daily":
+          prevHours.forEach((hour, index) => {
+            hour.innerText = `Yesterday - ${timeData[index].timeframes[timeFrame].previous}hrs`
+          })
+          break;
+        case "weekly":
+          prevHours.forEach((hour, index) => {
+            hour.innerText = `Last Week - ${timeData[index].timeframes[timeFrame].previous}hrs`
+          })
+          break;
+        case "monthly":
+          prevHours.forEach((hour, index) => {
+            hour.innerText = `Last Month - ${timeData[index].timeframes[timeFrame].previous}hrs`
+          })
+          break;
+      }
+    }
+    setTimeFrame("weekly")
+
+    function highlightsTimeFrame(time) {
+      [daily, weekly, monthly].forEach((a) => {
+        a.classList.remove("selected")
+      })
+      time.classList.add("selected")
+    }
+
+    daily.addEventListener("click", (e) => {
+      highlightsTimeFrame(e.target)
+      setTimeFrame("daily")
+    })
+    weekly.addEventListener("click", (e) => {
+      highlightsTimeFrame(e.target)
+      setTimeFrame("weekly")
+    })
+    monthly.addEventListener("click", (e) => {
+      highlightsTimeFrame(e.target)
+      setTimeFrame("monthly")
+    })
+
+
+}
