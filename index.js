fdc30ae348832e312b4ebf3ac316e867a7e189d5
Tobbies <85476101+tobbiesfake@users.noreply.github.com>
2022-04-06 11:41:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ window.onload = () =>{
     const daily = document.querySelector("#daily");
     const weekly = document.querySelector("#weekly");
     const monthly = document.querySelector("#monthly");
+    const categories = document.querySelectorAll(".category");
 
     (function () {
       titles.forEach ((title, index) => {
@@ -45,17 +46,26 @@ window.onload = () =>{
       time.classList.add("selected")
     }
 
+    function setAnimation(animated){
+      animated.forEach ((e) => {
+        e.classList.add("animation")
+        setTimeout(() => {e.classList.remove("animation")}, 500)
+      })
+    }
     daily.addEventListener("click", (e) => {
       highlightsTimeFrame(e.target)
       setTimeFrame("daily")
+      setAnimation(categories)
     })
     weekly.addEventListener("click", (e) => {
       highlightsTimeFrame(e.target)
       setTimeFrame("weekly")
+      setAnimation(categories)
     })
     monthly.addEventListener("click", (e) => {
       highlightsTimeFrame(e.target)
       setTimeFrame("monthly")
+      setAnimation(categories)
     })
 
 
